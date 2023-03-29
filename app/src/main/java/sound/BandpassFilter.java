53f6658a40d08ea3a62c509fee463147d90d2479
Andy <ajscottlewis@gmail.com>
2023-03-29 10:46:49 +0100

    Changes value below 40 to 40

--- app/src/main/java/sound/BandpassFilter.java
+++ app/src/main/java/sound/BandpassFilter.java
@@ -2,6 +2,11 @@ package sound;
 
 public class BandpassFilter {
     public int[] filterSoundwaves(int[] soundwaves) {
+        if (soundwaves[0] < 40) {
+            int[] soundwave = { 40 };
+            return soundwave;
+        }
+        
         return soundwaves;
     }
 }
--- app/src/test/java/sound/BandpassFilterTest.java
+++ app/src/test/java/sound/BandpassFilterTest.java
@@ -9,5 +9,12 @@ public class BandpassFilterTest {
         int[] soundwaves = { 100 };
         int[] expectedValues = { 100 };
         assertArrayEquals(bandpassFilter.filterSoundwaves(soundwaves), expectedValues);
-    }    
+    }
+
+    @Test public void testInputBelowValidRange() {
+        BandpassFilter bandpassFilter = new BandpassFilter();
+        int[] soundwaves = { 10 };
+        int[] expectedValues = { 40 };
+        assertArrayEquals(bandpassFilter.filterSoundwaves(soundwaves), expectedValues);
+    }
 }
